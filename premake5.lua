954fa6ae6ff5cde3d1f7d77b8ff119906e5e2571
qolisipo <57019940+qolisipo@users.noreply.github.com>
2021-12-06 12:03:32 +0000

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+	
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:MinSizeRelease"
+		runtime "Release"
+		optimize "On"
